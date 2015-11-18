5bbcbaf45f49fb4ba7bf0f075964883de1dda057
Nic La <sakeofmaking@gmail.com>
2015-11-17 16:37:08 -0800

    Create drvreg.h

---
+++ drvreg.h
@@ -0,0 +1,31 @@
+/* Port A */
+#define PINA 0x20
+#define DDRA 0x21
+#define PORTA 0x22
+
+/* Port B */
+#define PINB 0x23
+#define DDRB 0x24
+#define PORTB 0x25
+
+/* Timer 0 */
+#define OCR0A 0x47
+#define OCR0B 0x48
+#define TCCR0A 0x44
+#define COM0A1 7
+#define COM0A0 6
+#define COM0B1 5
+#define COM0B0 4
+#define WGM01 1
+#define WGM00 0
+#define TCCR0B 0x45
+#define CS02 2 
+#define CS00 0
+#define TCNT0 0x46
+#define TIFR0 0x35
+#define OCF0B 2
+#define OCF0A 1
+#define TOV0 0
+#define TIMSK0 0x6E
+#define TOIE0 0
+
